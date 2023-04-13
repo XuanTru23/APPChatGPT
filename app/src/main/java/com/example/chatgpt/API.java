6b6f690542bdcfd7cdf906d74acaf3d14c19c031
Trần Xuân Trữ <117728605+XuanTru23@users.noreply.github.com>
2023-04-14 02:32:45 +0700

    Hoan thanh

---
+++ app/src/main/java/com/example/chatgpt/API.java
@@ -0,0 +1,6 @@
+package com.example.chatgpt;
+
+public class API {
+    public static String API_URL = "https://api.openai.com/v1/completions";
+    public static String API = "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";
+}
